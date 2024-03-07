6d9417f139e49299be77e782ef8f8632c6ba23e3
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-03-07 14:53:17 +0300

    chore: create account screen

--- app/screens/AccountScreen.tsx
+++ app/screens/AccountScreen.tsx
@@ -1,12 +1,78 @@
-import { Text } from "@gluestack-ui/themed";
+import {
+  Avatar,
+  AvatarFallbackText,
+  Box,
+  Divider,
+  VStack,
+  ChevronRightIcon,
+  Icon,
+} from "@gluestack-ui/themed";
 import Screen from "../components/Screen";
+import { Text } from "@gluestack-ui/themed";
+import { StyleSheet, TouchableHighlight } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import colors from "../utils/colors";
 
 const AccountScreen = () => {
   return (
     <Screen>
-      <Text>Account Screen</Text>
+      <VStack>
+        <Box style={styles.avatarContainer}>
+          <Avatar bgColor="$green700" size="lg" borderRadius="$full">
+            <AvatarFallbackText>Kasili Wachiye</AvatarFallbackText>
+          </Avatar>
+          <Box ml="$4">
+            <Text bold>Kasili Wachiye</Text>
+            <Text size="sm">wachiye25@gmail.com</Text>
+          </Box>
+        </Box>
+        <Divider mt="$2" />
+        <TouchableHighlight
+          underlayColor={colors.light}
+          onPress={() => console.log("Signed out")}
+        >
+          <Box style={styles.logoutContainer} py="$2">
+            <Box style={styles.iconContainer}>
+              <Avatar bgColor="$red600" size="lg" borderRadius="$full">
+                <MaterialCommunityIcons
+                  name={"logout"}
+                  color={"white"}
+                  size={20}
+                />
+              </Avatar>
+              <Text ml="$4" bold>
+                Logout
+              </Text>
+            </Box>
+            <Box ml="$4">
+              <Icon as={ChevronRightIcon} size="lg" color="black" />
+            </Box>
+          </Box>
+        </TouchableHighlight>
+      </VStack>
     </Screen>
   );
 };
 
 export default AccountScreen;
+
+const styles = StyleSheet.create({
+  avatarContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 4,
+  },
+  iconContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 4,
+  },
+  logoutContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
---
+++ app/utils/colors.ts
@@ -0,0 +1,10 @@
+export default {
+  primary: "#66cc8a", // Primary color in emerald theme
+  secondary: "#333c4d", // Secondary color in emerald theme
+  black: "#000",
+  white: "#fff",
+  medium: "#8c8c8c", // A slightly darker shade of gray
+  light: "#f0f0f0", // A lighter shade of gray
+  danger: "#dc2626", // Danger color in emerald theme
+  dark: "#111827", // Dark color in emerald theme
+};
