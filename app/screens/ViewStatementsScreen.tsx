b18de0d4e1eed84658676d4e9d8cfb29e2d883e7
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-03-18 16:43:40 +0300

    chore: create view statements screen

--- app/screens/ViewStatementsScreen.tsx
+++ app/screens/ViewStatementsScreen.tsx
@@ -1,16 +1,84 @@
 import React from "react";
-import { Text } from "@gluestack-ui/themed";
-import { StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList, TouchableOpacity } from "react-native";
 import Screen from "../components/Screen";
+import colors from "../utils/colors";
+import { Text } from "@gluestack-ui/themed";
 
-const ViewStatementsScreen = () => {
+interface Transaction {
+  id: string;
+  date: string;
+  entity: string;
+  amount: number;
+  time: string;
+  description: string;
+}
+
+// Dummy data for the list
+const transactions: Transaction[] = [
+  {
+    id: "t1",
+    date: "20 March 2024",
+    entity: "Optiven Limited",
+    amount: -250000.0,
+    time: "07:58 AM",
+    description: "Installment",
+  },
+  {
+    id: "t2",
+    date: "20 February 2024",
+    entity: "Optiven Limited",
+    amount: -250000.0,
+    time: "07:08 PM",
+    description: "Installment",
+  },
+];
+
+const ViewStatement: React.FC = () => {
+  const renderTransaction = ({ item }: { item: Transaction }) => {
+    return (
+      <TouchableOpacity
+        style={styles.transactionItem}
+        onPress={() => console.log("Navigate to detail screen", item)}
+      >
+        <View style={styles.transactionDetails}>
+          <Text bold>{item.entity}</Text>
+          <Text size="sm">{item.date}</Text>
+        </View>
+        <View style={styles.transactionAmount}>
+          <Text color="tomato">{item.amount}</Text>
+          <Text size="sm" color={colors.medium}>
+            {item.time}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <Screen>
-      <Text>View Statements Screen</Text>
+      <FlatList
+        data={transactions}
+        renderItem={renderTransaction}
+        keyExtractor={(item) => item.id}
+      />
     </Screen>
   );
 };
 
-export default ViewStatementsScreen;
+export default ViewStatement;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  transactionItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: colors.medium,
+  },
+  transactionDetails: {
+    justifyContent: "space-between",
+  },
+  transactionAmount: {
+    alignItems: "flex-end",
+  },
+});
