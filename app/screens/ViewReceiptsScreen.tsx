03d3e7a9ab3aca165b51b0e6ab9b8178fdf73cd0
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-04-17 12:55:10 +0300

    fix: remove on press prop in the receipt item component

--- app/screens/ViewReceiptsScreen.tsx
+++ app/screens/ViewReceiptsScreen.tsx
@@ -11,9 +11,11 @@ import {
   DownloadIcon,
   View,
 } from "@gluestack-ui/themed";
+import colors from "../utils/colors";
 
 // Assume this type represents the structure of your receipt data
 interface Receipt {
+  id: number;
   date: string;
   receiptNumber: string;
   projectName: string;
@@ -24,11 +26,68 @@ interface Receipt {
 // Sample data to simulate fetching receipts
 const sampleReceipts: Receipt[] = [
   {
+    id: 1,
     date: "Feb 19, 2024",
     receiptNumber: "REC017477",
     projectName: "Success Gardens (Phase 2)",
     plotNumber: "SP95",
     amount: 1405150.0,
+  },
+  {
+    id: 2,
+    date: "Mar 15, 2024",
+    receiptNumber: "REC018232",
+    projectName: "Dream Villa Estates",
+    plotNumber: "DV45",
+    amount: 950000.0,
+  },
+  {
+    id: 3,
+    date: "Apr 5, 2024",
+    receiptNumber: "REC019006",
+    projectName: "Lakeview Heights",
+    plotNumber: "LV12",
+    amount: 2100000.0,
+  },
+  {
+    id: 4,
+    date: "May 20, 2024",
+    receiptNumber: "REC020145",
+    projectName: "Sunset Gardens",
+    plotNumber: "SG32",
+    amount: 1789000.0,
+  },
+  {
+    id: 5,
+    date: "Jun 8, 2024",
+    receiptNumber: "REC021009",
+    projectName: "Pinecrest Residences",
+    plotNumber: "PR27",
+    amount: 1350000.0,
+  },
+  {
+    id: 6,
+    date: "Jul 14, 2024",
+    receiptNumber: "REC021745",
+    projectName: "Oceanview Apartments",
+    plotNumber: "OA18",
+    amount: 1925000.0,
+  },
+  {
+    id: 7,
+    date: "Aug 9, 2024",
+    receiptNumber: "REC022512",
+    projectName: "Golden Meadows",
+    plotNumber: "GM73",
+    amount: 1258000.0,
+  },
+  {
+    id: 8,
+    date: "Sep 3, 2024",
+    receiptNumber: "REC023389",
+    projectName: "Silver Springs",
+    plotNumber: "SS56",
+    amount: 1850000.0,
   },
 ];
 
@@ -77,11 +136,10 @@ const ViewReceiptsScreen = () => {
   return (
     <Screen>
       <FlatList
-        data={receipts}
+        data={receipts.sort((a: any, b: any) => b.id - a.id)}
         renderItem={({ item }) => (
           <ReceiptItem
             receipt={item}
-            onPress={() => handlePressReceipt(item)}
           />
         )}
         keyExtractor={(item) => item.receiptNumber}
@@ -96,8 +154,8 @@ const styles = StyleSheet.create({
   receiptItem: {
     padding: 16,
     borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-    backgroundColor: "#fff",
+    borderBottomColor: colors.light,
+    backgroundColor: colors.white,
   },
   receiptHeader: {
     flexDirection: "row",
