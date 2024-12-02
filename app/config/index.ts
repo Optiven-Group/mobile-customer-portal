89eeb27108a2747f19871e4d3cb953c6f2ade477
Kasili <63935096+kasiliwachiye@users.noreply.github.com>
2024-12-02 10:33:15 +0300

    fix: removed environment-based BASE_URL logic and replaced with a static URL

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,18 +1,1 @@
-import Constants from "expo-constants";
-
-// const ENV = Constants.expoConfig?.extra?.env || "development";
-const ENV = "production";
-
-const getBaseUrl = () => {
-  if (ENV === "production" || ENV === "staging") {
-    // Use the deployed server IP address for production and staging
-    return "http://167.99.80.202";
-  } else if (Constants.expoConfig?.hostUri) {
-    // Continue to use the Expo-hosted local IP for development on LAN
-    return `http://${Constants.expoConfig.hostUri.split(":").shift()}:8080`;
-  } else {
-    return "http://192.168.0.152:8080"; // Default fallback for development
-  }
-};
-
-export const BASE_URL = getBaseUrl();
+export const BASE_URL = "https://optivenconnect.optiven.co.ke";
